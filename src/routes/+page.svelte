7b00d994bcdd5928946152f8641540fe154ecbb3
Marountas John <marjohn@imbb.forth.gr>
2022-09-06 10:11:23 +0300

    added an empty list and some styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+
+
+</script>
+
 <h1>Welcome to SvelteKit and EdgeDB</h1>
 <p>To read the documentation please visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> and <a href="https://www.edgedb.com/docs">www.edgedb.com/docs</a>. </p>
 
@@ -6,5 +11,21 @@
 
 
 <h2>Available Movies</h2>
-<p>Below you will find a list of movies that are available in our EdgeDB</p>
+<p>Below you will find a list of movies that are available in our EdgeDB:</p>
 
+<ul>
+	<li><span class="error">No movies found :(</span></li>
+</ul>
+
+<style>
+	li{
+		background-color: #fff;
+		border:1px solid #efefef;
+		margin-bottom:5px;
+		padding:10px;
+	}
+
+	.error{
+		color:crimson;
+	}
+</style>
